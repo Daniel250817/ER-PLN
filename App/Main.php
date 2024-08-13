f2bfb1b23917512ed58e2f394ae35cc2579cfabe
Julio <juliomix089@gmail.com>
2024-08-13 14:45:09 -0600

    :poop: Cambios no funcionales

--- App/Main.php
+++ App/Main.php
@@ -6,42 +6,33 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" type="text/css" href="Styles.css">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
-
     <title>Diagram ER</title>
-
-    <div class="Toogle" onclick="window.location.href='?toggle_dark_mode=1'">
-        Dark Mode
-        <span class="material-symbols-outlined">dark_mode</span>
-    </div>
-
 </head>
 <body class="<?php echo $dark_mode ? 'dark-mode' : ''; ?>">
+    
+
     <div class="header">
         <h1>Diagrama ER</h1>
+        <div class="Toogle" onclick="window.location.href='?toggle_dark_mode=1'">
+        <p>Dark Mode</p>
+        <span class="material-symbols-outlined">dark_mode</span>
+    </div>
     </div>
   
-    <div class="Container">
-        <section>
-            <h2>Tabla Alumnos</h2>
-            <table>
-                <tr>
-                    <th>Nombre</th>
-                    <th>Tipo de documento</th>
-                    <th>Nacionalidad</th>
-                </tr>
-                <tr>
-                    <td>Columna 1</td>
-                    <td>Columna 2</td>
-                    <td>Columna 3</td>
-                </tr>
-            </table>
+    <div class="Container-Left">
+        <section class="Section-Left">
+            <h2>Sección izquierda</h2>
         </section>
-    </div>    
-
-</body>
-<footer>
-        <div class = "Footer-Container">
+    </div>
+    <div class="Container-Right">
+        <section class="Section-Right">
+            <h2>Sección derecha</h2>
+        </section>
+    </div> 
+    <footer>
+        <div class="Footer-Container">
             <h1>Este es el pie de página</h1>
         </div>   
- </footer>
+    </footer> 
+</body>
 </html>
